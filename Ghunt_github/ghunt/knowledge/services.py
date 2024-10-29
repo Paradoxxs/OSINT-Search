57ec18f58062a12aee4e1467c35cf02c58022524
Paradoxxs <Aston.dk@gmail.com>
2024-10-29 11:05:36 +0100

    rremove ghunt test

--- Ghunt_github/ghunt/knowledge/services.py
+++
@@ -1,4 +0,0 @@
-services_baseurls = {
-    "cloudconsole": "console.cloud.google.com",
-    "cl": "calendar.google.com"
-}
--- Ghunt_github/ghunt/version.py
+++
@@ -1,4 +0,0 @@
-metadata = {
-    "version": "2.1.0",
-    "name": "BlackHat Edition"
-}
